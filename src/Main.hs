57726af44d50d341ad7ebad8e95dbf80f44f2f61
Tina Wuest <tina@wuest.me>
2018-04-04 10:04:23 -0500

    Working color fill
    Committing for the sake of posterity.  Very ugly commit, but working
    color fill, with color differentiation for page 1 vs page 2.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,22 @@
 module Main where
 
 import qualified Opts
-import qualified Streamdeck as SD
+import qualified Data.ByteString as BS
+import qualified Streamdeck      as SD
 
 import Prelude
+
+solidBlue :: Integer -> BS.ByteString
+solidBlue 1 = BS.pack $ take (3 * 2583) (cycle [0xff, 0x00, 0x00])
+solidBlue 2 = BS.pack $ take (3 * 2601) (cycle [0xff, 0x00, 0xff])
+solidBlue _ = BS.pack []
 
 main :: IO ()
 main = do
     _     <- Opts.getOpts
     decks <- SD.enumerateStreamDecks
+    deck  <- SD.openStreamDeck $ decks !! 0
+    _ <- SD.send deck SD.initializationReport
+    _ <- SD.writePage deck 1 0 $ solidBlue 1
+    _ <- SD.writePage deck 2 0 $ solidBlue 2
     putStrLn $ decks >>= show
--- src/Streamdeck.hs
+++ src/Streamdeck.hs
@@ -1,10 +1,14 @@
 module Streamdeck ( Deck (..)
+                  , openStreamDeck
                   , enumerateStreamDecks
                   , vendorID, productID
+                  , initializationReport
+                  , send, Streamdeck.read
+                  , writePage
                   ) where
 
 import qualified Data.ByteString as BS
-import qualified Data.Word       as DW   (Word16)
+import qualified Data.Word       as DW   (Word16, Word8)
 import qualified System.HIDAPI   as HID
 
 import Prelude
@@ -33,6 +37,47 @@ vendorID  = 0x0fd9
 
 productID :: DW.Word16
 productID = 0x0060
+
+packetSize :: Int
+packetSize = 4096
+
+initializationReport :: BS.ByteString
+initializationReport = BS.pack [ 0x05                   -- Report 0x05
+                               , 0x55, 0xAA, 0xD1, 0x01 -- Command
+                               , 0x63, 0x00, 0x00, 0x00 -- 0x63 (99)% brightness
+                               , 0x00, 0x00, 0x00, 0x00
+                               , 0x00, 0x00, 0x00, 0x00
+                               ]
+
+send :: Deck -> BS.ByteString -> IO Int
+send deck bs = 
+    case BS.length bs > packetSize of
+        True -> do
+            _ <- HID.write (ref deck) $ BS.take packetSize bs
+            send deck $ BS.drop packetSize bs
+        False -> HID.write (ref deck) bs
+
+writePage :: Deck -> Int -> DW.Word8 -> BS.ByteString -> IO Int
+writePage deck p i bs = send deck $ BS.append (page p i) bs
+
+page :: Int -> DW.Word8 -> BS.ByteString
+page 1 i = BS.pack [ 0x02, 0x01, 0x01, 0x00, 0x00, (i+1), 0x00, 0x00
+                   , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+                   , 0x42, 0x4d, 0xf6, 0x3c, 0x00, 0x00, 0x00, 0x00
+                   , 0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00
+                   , 0x00, 0x00, 0x48, 0x00, 0x00, 0x00, 0x48, 0x00
+                   , 0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00
+                   , 0x00, 0x00, 0xc0, 0x3c, 0x00, 0x00, 0xc4, 0x0e
+                   , 0x00, 0x00, 0xc4, 0x0e, 0x00, 0x00, 0x00, 0x00
+                   , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+page 2 i = BS.pack [ 0x02, 0x01, 0x02, 0x00, 0x01, (i+1), 0x00, 0x00
+                   , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+page _ _ = BS.pack []
+
+read :: Deck -> Int -> IO BS.ByteString
+read deck c = HID.read (ref deck) c
 
 -- DeviceInfo { path = "/dev/hidraw%d"
 --            , vendorId = 4057
