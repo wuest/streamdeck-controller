ac81e0798fe9b23deb8a9feefc7b3106c76eba52
Tina Wuest <tina@wuest.me>
2018-04-07 23:04:02 -0500

    Implement Wai for configurator work
    Next steps:
    * Build configurator
    * Implement image processing for bidirectional image fetch/write
    * Design configuration file format

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
 import qualified Opts
-import qualified Streamdeck as SD
+import qualified Streamdeck               as SD
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai              as Wai
+
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Char8 as BS
+import qualified Network.HTTP.Types             as Http
 
 import Prelude
 
@@ -11,6 +17,18 @@ readForever deck = do
     putStrLn $ show $ SD.buttonPressed newDeck 0
     readForever deck
 
+
+textPlain :: Http.Header
+textPlain = (Http.hContentType, BS.pack "text/plain")
+
+static :: String -> Wai.Application
+static s request respond = respond $ case Wai.rawPathInfo request of
+    _             -> Wai.responseLBS Http.status200 [textPlain] (LBS.fromStrict $ BS.pack s)
+
+
+--basicApp :: String -> Wai.Application
+--basicApp s r f = Wai.responseLBS Http.status200 [(Http.hContentType, BS.pack "text/plain")] 
+
 main :: IO ()
 main = do
     _     <- Opts.getOpts
@@ -19,6 +37,7 @@ main = do
     SD.send deck SD.initializationReport
     _ <- SD.updateDeck deck id
 
-    readForever deck
+    putStrLn $ show $ head decks
 
-    putStrLn $ decks >>= show
+    let settings = Warp.setPort 3333 Warp.defaultSettings
+    Warp.runSettings settings $ static (show $ head decks)
