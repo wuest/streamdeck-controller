f463e7015ab82c897298ce1c6c33279a81c13d10
Tina Wuest <tina@wuest.me>
2018-04-10 03:31:38 -0500

    Add skeleton elm application

---
+++ src/elm/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (main)
+
+import Html            as H
+import Html.Events     as Event
+import Html.Attributes as Attr
+
+type Msg = String
+
+type alias Model = {}
+
+type alias Flags = {}
+
+--main : Program Flags Model Msg
+--main = H.programWithFlags
+main : Program Never Model Msg
+main = H.program
+    { init          = init
+    , update        = update
+    , view          = view
+    , subscriptions = subscriptions
+    }
+
+--init : Flags -> (Model, Cmd Msg)
+--init _ = { } ! []
+init : (Model, Cmd Msg)
+init = { } ! []
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update _ _ = { } ! []
+
+view : Model -> H.Html Msg
+view _ =
+  H.div []
+    [ H.select []
+      (List.map (\n -> H.option [ Attr.value (toString n) ] [ H.text (toString n) ]) (List.range 1 12))
+    ]
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = Sub.none
