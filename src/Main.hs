5e921da0c53fcda995e7787892ee04e66a1acdf1
Tina Wuest <tina@wuest.me>
2019-08-29 21:07:22 +0300

    Add scotty+haskell+elm templating

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import qualified Opts
-import qualified Streamdeck               as SD
-import qualified Network.Wai.Handler.Warp as Warp
-import qualified Network.Wai              as Wai
+import Prelude
+import Control.Monad ( unless )
+import Control.Monad.Reader ( runReaderT )
 
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Char8 as BS
-import qualified Network.HTTP.Types             as Http
 
-import Prelude
+import Web.Scotty.Trans
+import Network.Wai.Middleware.Static ( addBase, hasPrefix, noDots, staticPolicy, (>->) )
+import Network.Wai.Handler.WebSockets ( websocketsOr )
+import Network.WebSockets ( defaultConnectionOptions )
+
+import qualified System.Directory as Dir
+
+import qualified Opts
+import qualified Routes
+import qualified Websockets as WS
+import qualified Streamdeck as SD
 
 readForever :: SD.Deck -> IO ()
 readForever deck = do
@@ -17,21 +27,32 @@ readForever deck = do
     putStrLn $ show $ SD.buttonPressed newDeck 0
     readForever deck
 
+start :: Int -> FilePath -> IO ()
+start port base = do
+    appState <- WS.initState
+    scottyT port (`runReaderT` base) $ do
+        middleware $ websocketsOr defaultConnectionOptions $ WS.app appState
+        middleware $ staticPolicy $ noDots >-> hasPrefix "static" >-> addBase base
+        Routes.routes
 
-textPlain :: Http.Header
-textPlain = (Http.hContentType, BS.pack "text/plain")
+setupDataDir :: FilePath -> IO ()
+setupDataDir path = do
+    exists <- Dir.doesDirectoryExist path
+    unless exists $ do
+        putStrLn $ "Data directory (" ++ path ++ ") doesn't exist - creating..."
+        Dir.createDirectory path
 
-static :: String -> Wai.Application
-static s request respond = respond $ case Wai.rawPathInfo request of
-    _             -> Wai.responseLBS Http.status200 [textPlain] (LBS.fromStrict $ BS.pack s)
-
-
---basicApp :: String -> Wai.Application
---basicApp s r f = Wai.responseLBS Http.status200 [(Http.hContentType, BS.pack "text/plain")] 
+    exists' <- Dir.doesDirectoryExist (path ++ "/static")
+    unless exists' $ do
+        putStrLn $ "Data directory (" ++ path ++ "/static) doesn't exist - creating..."
+        Dir.createDirectory (path ++ "/static")
 
 main :: IO ()
 main = do
-    _     <- Opts.getOpts
+    opts <- Opts.getOpts
+
+---
+
     decks <- SD.enumerateStreamDecks
     deck  <- SD.openStreamDeck $ head decks
     SD.send deck SD.initializationReport
@@ -39,5 +60,12 @@ main = do
 
     putStrLn $ show $ head decks
 
-    let settings = Warp.setPort 3333 Warp.defaultSettings
-    Warp.runSettings settings $ static (show $ head decks)
+---
+
+    let port = Opts.webPort opts
+    base <- Opts.baseDir opts
+    _ <- setupDataDir base
+    putStrLn $ "Starting service on port " ++ show port
+    start port base
+
+---    Warp.runSettings settings $ static (show $ head decks)
